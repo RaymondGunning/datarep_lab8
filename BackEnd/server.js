ad4ca8607a1aecbbca9245323bc4a2dca03b86e6
RaymondGunning <raymondgunning24@gmail.com>
2020-11-24 23:43:18 +0000

    lab complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 4000
-
 const cors = require('cors');// including cors
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 app.use(cors());
 
@@ -21,43 +21,81 @@ app.use(bodyParser.urlencoded({extended: false}))
 // parse app/json
 app.use(bodyParser.json())
 
+const myConnectionString = 'mongodb+srv://admin:admin@cluster0.3tu8b.mongodb.net/movies?retryWrites=true&w=majority';
+mongoose.connect(myConnectionString, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String
+});
+
+var MovieModel = mongoose.model("movie",movieSchema);
+
 app.post('/api/movies', (req, res) => {
   console.log('Movie Recieved');
   console.log(req.body.title);
   console.log(req.body.year);
   console.log(req.body.poster);
+
+  MovieModel.create({
+      title:req.body.title,
+      year:req.body.year,
+      poster:req.body.poster
+  })
+
+  res.send('Item Added');
+
 })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/movies',(req, res) => {
+app.get('/api/movies',(req, res) =>
+{
 
-  const mymovies = 
-  [
-    {
-      "Title":"Avengers: Infinity War",
-      "Year":"2018",
-      "imdbID":"tt4154756",
-      "Type":"movie",
-      "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-      },
-      {
-      "Title":"Captain America: Civil War",
-      "Year":"2016",
-      "imdbID":"tt3498820",
-      "Type":"movie",
-      "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-      },
+  // const mymovies = 
+  // [
+  //   {
+  //     "Title":"Avengers: Infinity War",
+  //     "Year":"2018",
+  //     "imdbID":"tt4154756",
+  //     "Type":"movie",
+  //     "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+  //     },
+  //     {
+  //     "Title":"Captain America: Civil War",
+  //     "Year":"2016",
+  //     "imdbID":"tt3498820",
+  //     "Type":"movie",
+  //     "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+  //     }
       
-  ]
+  // ];
 
-  res.status(200).json({
-    message: "Everything is ok",//passing information
-    movies:mymovies});             
+  MovieModel.find((err, data)=>{
+      res.json(data);
+  })
+
+  //   res.status(200).json({
+  //     message: "Everything is ok",//passing information
+  //     movies:mymovies});             
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+app.get('/api/movies/:id', (req,res)=>{
+    console.log(req.params.id);
+
+    MovieModel.findById(req.params.id,(err,data)=>{
+      res.json(data);
+    })
+})
+
+app.listen(port, () =>{
+
+  console.log('Example app listening at http://localhost:${port}')
+
 })
